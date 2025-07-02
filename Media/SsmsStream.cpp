63bd781bda4beaf4b821aaa97dd1f74020cb08c8
NN <2556780516@qq.com>
2025-07-03 07:01:58 +0800

    修复打印日志内容

--- Media/SsmsStream.cpp
+++ Media/SsmsStream.cpp
@@ -64,7 +64,7 @@ void SsmsStream::Pop(const SsmsPlayClientPtr &player)
         idx = gop_manage_->GetGopByLatency(content_latency);
         LOG_DEBUG << "packet expired or latency is too high, latest video frame timestamp " << gop_manage_->LatestTimeStamp()
                     << ", client out latest video frame timestamp " << player->out_video_timestamp_
-                    << ", latest packet index " << ExpiredPacketIndex() << ", client request packet index " << src_idx;
+                    << ", expired packet index max value " << ExpiredPacketIndex() << ", client request packet index " << src_idx;
     }
 
     //头部更新则发新的头部
